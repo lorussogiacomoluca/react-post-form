fb2f38dc95ad4b964f9bb4ce6ff73a0a8f2a7e62
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-14 15:30:34 +0200

    [UPD]  Created navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="container-fluid p-3 border-bottom ">
+        <div className="container">
+          <div className="row">
+            <h1 className="text-center">React Post Form</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
